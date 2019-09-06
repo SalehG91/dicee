7d47f6e02f4e9a524715b79d442bfc1ed5b9d7e4
SalehG91 <Saleh.g91@gmail.com>
2019-09-06 16:02:32 +0300

    First commit with Dicee completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,90 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.lightGreen[600],
+          appBar: AppBar(
+            title: Center(child: Text('Dicee')),
+            backgroundColor: Colors.lightBlue[900],
+          ),
+          body: DicePage(),
         ),
-        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+  int total;
+
+  void generateRandom() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      total = leftDiceNumber + rightDiceNumber;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+          Text(
+            'Click on a dice to roll!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                    onPressed: () {
+                      generateRandom();
+                      print('left button pressed!');
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png')),
+              ),
+              Expanded(
+                child: FlatButton(
+                    onPressed: () {
+                      generateRandom();
+                      print('right button pressed!');
+                    },
+                    child: Image.asset('images/dice$rightDiceNumber.png')),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'your total is: $total',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
